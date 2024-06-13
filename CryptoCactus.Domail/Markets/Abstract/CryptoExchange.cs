80c0d72c88804a82ac5209199c3178d7747f6423
Gotlib <mixalev702@mail.ru>
2024-06-13 22:57:05 +0300

    version: 1.2

---
+++ CryptoCactus.Domail/Markets/Abstract/CryptoExchange.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptoCactus.Domain.Markets.Abstract
+{
+    abstract public class CryptoExchange
+    {
+        public Dictionary<string, double> Currencies = new Dictionary<string, double>();
+        abstract public void GetCurrenciesByAPI(string apiKey, string apiSecret, Dictionary<string, double> Currencies);
+        abstract public void GetOnlyOneCurrencByAPI(string apiKey, string apiSecret, string nameOfCurrenc, Dictionary<string, double> Currencies);
+
+    }
+}
